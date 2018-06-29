abdd5caf31c43b0f8309996dc53d0224127b7aaa
aisav <artur.isaverdyan@gmail.com>
2018-06-29 23:23:26 +0500

    jquery & webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+window.$ = window.jQuery = require("jquery");
+
+// import $ from 'jquery';
+// console.log($)
+//
+// window.jQuery = $;
+// window.$ = $;
+
 import greet from'./greeting'
 
-console.log("Hello World")
 
 console.log(greet("Artur"))
+
+$('.title').html('Test')
+
+console.log("Hello World")
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+let path = require ('path')
+
+// new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery'
+// })
+
+let conf = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: "main.js",
+        publicPath: "dist/"
+    },
+    devServer: {
+        overlay: true
+    }
+}
+
+module.exports = conf
